0b0acf32dac019a7eb92de0eb69acc4ffaeebf83
omniops-ks <kevin.schrock@omniops.dev>
2023-03-06 12:11:12 -0700

    IAC commit again

--- docs/_site/assets/javascripts/lunr/lunr.store.js
+++ docs/_site/assets/javascripts/lunr/lunr.store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Omni Ops LLC - Who We Are and What We Do We are Omni Ops! A company that is using the power of AI to enhance your workflow. With the recent blow-up of OpenAI and their product ChatGPT, the opportunities to harness AI in everyday applications have never been more...","categories": ["about"],
         "tags": [],
         "url": "http://localhost:4000/about/2023/03/01/omni-ops-llc-using-ai-to-enhance-your-workflow-and-projects.html"
+      },{
+        "title": "Infrastructure as Code Generator: Project Breakdown",
+        "excerpt":"This post discusses the Infrastructure as Code generator that we will be building. Our goal is to test out the capabilities of OpenAI’s Codex Model’s to generate Ansible Playbooks from configurations pulled from devices. Sections Why build a Infrasturcutre as Code Generator Key Technologies in Use End Goal of this...","categories": ["projects","iac"],
+        "tags": [],
+        "url": "http://localhost:4000/projects/iac/2023/03/06/infrastructure-as-code-generator-project-breakdown.html"
       }]
